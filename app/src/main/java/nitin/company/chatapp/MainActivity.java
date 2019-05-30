eb5faeab2c14042ffeb5fc5e7a8e335fd55fde91
Nitin Pal <nitinluck0123@gmail.com>
2019-05-30 16:26:37 +0530

    reading message from database and showing on the interface fully implemented

--- app/src/main/java/nitin/company/chatapp/MainActivity.java
+++ app/src/main/java/nitin/company/chatapp/MainActivity.java
@@ -1,5 +1,7 @@
 package nitin.company.chatapp;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -16,6 +18,9 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -35,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton mPhotoPickerButton;
     private EditText mMessageEditText;
     private Button mSendButton;
+    private ChildEventListener mchildeventlistener;
 
     private String mUsername;
     @Override
@@ -104,6 +110,34 @@ public class MainActivity extends AppCompatActivity {
                 mMessageEditText.setText("");
             }
         });
+        mchildeventlistener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                FriendlyMessage friendlymessage = dataSnapshot.getValue(FriendlyMessage.class);
+                mMessageAdapter.add(friendlymessage);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+          myRef.addChildEventListener(mchildeventlistener);
     }
 
     @Override
